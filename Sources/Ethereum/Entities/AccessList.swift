0b177263a36291d003740866c05078564bbfeefc
andooown <andooown.dev@gmail.com>
2023-02-15 23:30:57 +0900

    `AccessList` uses `Hash`

--- Sources/Ethereum/Entities/AccessList.swift
+++ Sources/Ethereum/Entities/AccessList.swift
@@ -3,11 +3,11 @@ import Foundation
 public struct AccessList: RLPEncodable {
     public struct Element: RLPEncodable {
         public let address: Address
-        public let storageKeys: [[UInt8]]
+        public let storageKeys: [Hash]
 
         public init(
             address: Address,
-            storageKeys: [[UInt8]]
+            storageKeys: [Hash]
         ) {
             self.address = address
             self.storageKeys = storageKeys
@@ -16,7 +16,7 @@ public struct AccessList: RLPEncodable {
         public func encodeToRLP() throws -> Data {
             try [
                 AnyRLPEncodable(address),
-                AnyRLPEncodable(storageKeys.map { Data($0) }),
+                AnyRLPEncodable(storageKeys),
             ].encodeToRLP()
         }
     }
