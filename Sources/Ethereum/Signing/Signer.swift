3c657408034666f2bcd4347cbd69ab635a4416ec
andooown <andooown.dev@gmail.com>
2023-02-15 23:12:06 +0900

    Build type=2 tx

--- Sources/Ethereum/Signing/Signer.swift
+++ Sources/Ethereum/Signing/Signer.swift
@@ -5,39 +5,33 @@ import Foundation
 public protocol Signer {
     var chainID: BigInt { get }
 
-    func hash(tx: Transaction) throws -> [UInt8]
-    func signatureValues(from signature: [UInt8]) throws -> (r: BigInt, s: BigInt, v: BigInt)
+    func signatureValues(
+        from signature: [UInt8],
+        for txType: TransactionType
+    ) throws -> (r: BigInt, s: BigInt, v: BigInt)
 }
 
-public struct LegacyTxSigner: Signer {
+public struct LondonSigner: Signer {
     public let chainID: BigInt
 
     public init(chainID: BigInt) {
         self.chainID = chainID
     }
 
-    public func hash(tx: Transaction) throws -> [UInt8] {
-        let encoded = try [
-            AnyRLPEncodable(tx.nonce),
-            AnyRLPEncodable(tx.gasPrice),
-            AnyRLPEncodable(tx.gas),
-            AnyRLPEncodable(tx.to),
-            AnyRLPEncodable(tx.value),
-            AnyRLPEncodable(tx.data),
-            AnyRLPEncodable(chainID),
-            AnyRLPEncodable(0),
-            AnyRLPEncodable(0),
-        ].encodeToRLP()
-        let digest = SHA3(variant: .keccak256).calculate(for: encoded.bytes)
-
-        return digest
-    }
-
-    public func signatureValues(from signature: [UInt8]) throws -> (r: BigInt, s: BigInt, v: BigInt)
-    {
+    public func signatureValues(
+        from signature: [UInt8],
+        for txType: TransactionType
+    ) throws -> (r: BigInt, s: BigInt, v: BigInt) {
         let r = BigInt(sign: .plus, magnitude: BigUInt(Data(signature[0..<32])))
         let s = BigInt(sign: .plus, magnitude: BigUInt(Data(signature[32..<64])))
         let v = BigInt(sign: .plus, magnitude: BigUInt(Data([signature[64]])))
-        return (r, s, 2 * chainID + 35 + v)
+
+        switch txType {
+        case .legacy:
+            return (r, s, 2 * chainID + 35 + v)
+
+        case .dynamicFee:
+            return (r, s, v)
+        }
     }
 }
